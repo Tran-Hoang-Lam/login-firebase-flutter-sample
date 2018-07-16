f2db26048285d3596de56df86c8e23b9eb74bbb0
Tran-Hoang-Lam <Lam.Hoang.Tran@mgm-tp.com>
2018-07-17 01:01:29 +0700

    rename package

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_firebase_flutter_example/Screen/login_screen.dart';
+import 'package:login_firebase_flutter_example/screen/login_screen.dart';
 
 class App extends StatelessWidget {
   final appRoutes = <String, WidgetBuilder>{
