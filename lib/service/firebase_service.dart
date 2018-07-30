af17c5b63a73154090362ef4f928814a98fe1d30
Tran-Hoang-Lam <lam.hoang.tran@mgm-tp.com>
2018-07-30 17:14:09 +0700

    fix firebase storage

--- lib/service/firebase_service.dart
+++ lib/service/firebase_service.dart
@@ -7,8 +7,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:login_firebase_flutter_example/configuration/application_configuration.dart';
 
 class FirebaseService {
-  static FirebaseDatabase firebaseDatabase;
-  static FirebaseStorage firebaseStorage;
 
   final Future<FirebaseApp> firebaseApp = FirebaseApp.configure(
       name: 'db',
@@ -20,22 +18,19 @@ class FirebaseService {
           storageBucket: ApplicationConfiguration.configMap['storageBucket'],
           databaseURL: ApplicationConfiguration.configMap['databaseURL']));
 
-  initDatabase() {
-    firebaseApp.then((app) {
-      firebaseDatabase = FirebaseDatabase(app: FirebaseApp(name: 'db'));
-      firebaseStorage = FirebaseStorage(app: FirebaseApp(name: 'db'));
-    });
+  initDatabase() async {
+    await firebaseApp;
   }
 
   static Future<String> uploadFile(File file) async {
     try {
       if (!file.path.startsWith("asset")) {
-        String path = file.path.substring(file.path.lastIndexOf("/") + 1);
-        StorageUploadTask uploadTask =
-            firebaseStorage.ref().child('images').child(path).putFile(file);
+        final String path = file.path.substring(file.path.lastIndexOf("/") + 1).toLowerCase();
+        final StorageUploadTask uploadTask =
+        FirebaseStorage.instance.ref().child('images/$path').putFile(file);
 
-        UploadTaskSnapshot snapshot = await uploadTask.future;
-        Uri downloadUri = snapshot.downloadUrl;
+        final UploadTaskSnapshot snapshot = await uploadTask.future;
+        final Uri downloadUri = snapshot.downloadUrl;
         return downloadUri.toString();
       } else {
         return '';
